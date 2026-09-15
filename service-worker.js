4fff72539801032a12255877a51749cd6aa07e7f
Koozaa15 <benzsurasakhd@gmail.com>
2026-09-15 12:05:33 +0700

    Refresh app immediately after deployment

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
-self.addEventListener('install',event=>event.waitUntil(caches.open('eguestbook-v3').then(cache=>cache.addAll(['/','/index.html','/manifest.webmanifest','/icon.svg']))));self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!=='eguestbook-v3').map(key=>caches.delete(key))))));self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
+const CACHE='eguestbook-v4';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('/index.html'))));
+});
