3da495d687ea7b0fe2d094a91466b8d50e5261a1
Koozaa15 <benzsurasakhd@gmail.com>
2026-09-15 12:39:13 +0700

    Create elegant wedding welcome experience

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='eguestbook-v4';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg'];
+const CACHE='eguestbook-v5';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg','/wedding.css','/wedding.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
